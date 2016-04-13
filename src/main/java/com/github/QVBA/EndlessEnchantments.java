147e310c41d58b44e05f4737527fb2890c61e7b1
QVBA <littlekicker111@outlook.com>
2016-04-13 14:06:40 +0100

    Base mod, no functionality yet

---
+++ src/main/java/com/github/QVBA/EndlessEnchantments.java
@@ -0,0 +1,28 @@
+package com.github.QVBA;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(name = Reference.MOD_NAME, modid = Reference.MOD_ID, version = Reference.MOD_VERSION)
+public class EndlessEnchantments {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+
+}
---
+++ src/main/java/com/github/QVBA/Reference.java
@@ -0,0 +1,9 @@
+package com.github.QVBA;
+
+public class Reference {
+	
+	public static final String MOD_NAME = "EndlessEnchantments";
+	public static final String MOD_ID = "EndlessEnchantments";
+	public static final String MOD_VERSION = "inDev";
+
+}
